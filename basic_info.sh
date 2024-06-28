ab071457881417a99c0265acc980e80f6de19c76
hammad <hammad@192.168.10.13>
2024-06-28 16:27:17 +0500

    Add basic_info.sh script: Display system information

---
+++ basic_info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Name
+NAME="Hammad Kamran"
+
+## Get CPU information
+CPU_INFO=$(cat /proc/cpuinfo | grep 'model name' | uniq | awk -F ': ' '{print $2}')
+
+# Get RAM information
+RAM_TOTAL=$(free -h | awk '/^Mem/ {print $2}')
+
+# Get HDD information
+HDD_TOTAL=$(df -h / | awk 'NR==2 {print $2}')
+
+# Print system information
+echo "Name: $NAME"
+echo "CPU: $CPU_INFO"
+echo "RAM: $RAM_TOTAL"
+echo "HDD/SSD Capacity: $HDD_TOTAL"
